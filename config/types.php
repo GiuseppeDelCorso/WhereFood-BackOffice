b18c6cf6712b0e4e58369640765110cfe3589749
michelepiccinno <michele.piccinno@gmail.com>
2024-02-28 11:28:48 +0100

    fix types seeder

---
+++ config/types.php
@@ -0,0 +1,36 @@
+<?php
+  $types = [
+    [
+        'name' => 'primi',
+        'description' => 'primi piatti'
+    ],
+    [
+        'name' => 'secondi',
+        'description' => 'secondi piatti'
+    ],
+    [
+        'name' => 'desserts',
+        'description' => 'frutta e dolci'
+    ],
+    [
+        'name' => 'pizze',
+        'description' => 'pizze'
+    ],
+    [
+        'name' => 'panini',
+        'description' => 'panini'
+    ],
+    [
+        'name' => 'vini',
+        'description' => 'vini'
+    ],
+    [
+        'name' => 'birre',
+        'description' => 'birre'
+    ],
+    [
+        'name' => 'bibite',
+        'description' => 'bibite'
+    ],
+];
+return ($types);
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -10,44 +10,16 @@ class TypeSeeder extends Seeder
 {
     public function run(): void
     {
-        $types = [
-            [
-                'name' => 'primi',
-                'description' => 'primi piatti'
-            ],
-            [
-                'name' => 'secondi',
-                'description' => 'secondi piatti'
-            ],
-            [
-                'name' => 'desserts',
-                'description' => 'frutta e dolci'
-            ],
-            [
-                'name' => 'pizze',
-                'description' => 'pizze'
-            ],
-            [
-                'name' => 'panini',
-                'description' => 'panini'
-            ],
-            [
-                'name' => 'vini',
-                'description' => 'vini'
-            ],
-            [
-                'name' => 'birre',
-                'description' => 'birre'
-            ],
-            [
-                'name' => 'bibite',
-                'description' => 'bibite'
-            ],
-        ];
+        $types = config('types');
 
         foreach ($types as $type) {
-            $newType = new Type();
-            $newType->fill($type)->save();
+            $newtype = new Type();
+            $newtype->fill($type)->save();
         };
     }
 }
+
+
+
+
+
