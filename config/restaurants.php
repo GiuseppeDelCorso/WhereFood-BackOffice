2cfde9c81902618c112a74698c94dc59f0f97a6b
andreafrancioni <andreafrancioni.work@gmail.com>
2024-02-28 12:29:02 +0100

    fix user_id on restaurants.php config

--- config/restaurants.php
+++ config/restaurants.php
@@ -9,91 +9,91 @@ $restaurants = [
     'PIVA' => '10214930967'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 2,
     'name' => 'BurgerKing',
     'image' => 'immagine_burgerking',
     'address' => 'Via Mazzini 54',
     'PIVA' => '90214930960'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 14,
     'name' => 'FiveGuys',
     'image' => 'immagine_FiveGuys',
     'address' => 'Via Alessandria 2',
     'PIVA' => '90214930960'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 3,
     'name' => 'Pizzeria da Gigi',
     'image' => 'immagine_pizzeriagigi',
     'address' => 'Piazza Roma 45',
     'PIVA' => '75214960961'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 4,
     'name' => 'Steak House',
     'image' => 'immagine_steakhouse',
     'address' => 'Via Lucca 45',
     'PIVA' => '95214961967'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 5,
     'name' => 'La Taverna Del Pescatore',
     'image' => 'immagine_tavernapescatore',
     'address' => 'Viale Quarta 98',
     'PIVA' => '19814981944'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 10,
     'name' => 'Il Taco da Tuco',
     'image' => 'immagine_Il Taco_da_Tuco',
     'address' => 'Viale Quarta 98',
     'PIVA' => '19814981944'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 11,
     'name' => 'la Gelateria',
     'image' => 'immagine_la_gelateria',
     'address' => 'Viale Quarta 98',
     'PIVA' => '19814981944'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 12,
     'name' => 'Kebab Di Gaza',
     'image' => 'immagine_Kebab_Di_Gaza',
     'address' => 'Viale Quarta 98',
     'PIVA' => '19814981944'
   ],
   [
-    'user_id' => 1,
-    'name' => ' Evriday Thai',
+    'user_id' => 13,
+    'name' => ' Everyday Thai',
     'image' => 'immagine_Evriday_thai',
     'address' => 'Viale Quarta 98',
     'PIVA' => '19814981944'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 6,
     'name' => 'Il Kebab di Mubarak',
     'image' => 'immagine_kebabmubarak',
     'address' => 'Vico dei tori 56',
     'PIVA' => '87314981555'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 7,
     'name' => 'Hokkaido',
     'image' => 'immagine_hokkaido',
     'address' => 'Viale Umbria 13',
     'PIVA' => '11224981557'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 8,
     'name' => 'Ristorante Vegano Piccapane',
     'image' => 'immagine_piccapane',
     'address' => 'Viale del Salento 21',
     'PIVA' => '55524981575'
   ],
   [
-    'user_id' => 1,
+    'user_id' => 9,
     'name' => 'Ristorante Cor di zia',
     'image' => 'immagine_cor di zia',
     'address' => 'Viale abrosia',
