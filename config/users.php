3b44b0bbefcbf8ee6ccb9e87abd7e995ea981e4f
michelepiccinno <michele.piccinno@gmail.com>
2024-02-28 10:59:30 +0100

    add users seeder

---
+++ config/users.php
@@ -0,0 +1,40 @@
+<?php
+$users = [
+  [
+    'name' => 'Mario',
+    'email' => 'mcdonalds@gmail.it',
+  ],
+  [
+    'name' => 'Anna',
+    'email' => 'burgerKing@gmail.it',
+  ],
+  [
+    'name' => 'Luigi',
+    'email' => 'pizzeriadagigi@gmail.it',
+  ],
+  [
+    'name' => 'Carlo',
+    'email' => 'steakhouse@gmail.it',
+  ],
+  [
+    'name' => 'Pasquale',
+    'email' => 'latavernadelpescatore@gmail.it',
+  ],
+  [
+    'name' => 'Mubarak',
+    'email' => 'ilkebabdimubarak@gmail.it',
+  ],
+  [
+    'name' => 'Chongli',
+    'email' => 'hokkaido@gmail.it',
+  ],
+  [
+    'name' => 'Pina',
+    'email' => 'ristoranteveganopiccapane@gmail.it',
+  ],
+  [
+    'name' => 'Carmela',
+    'email' => 'ristorantecordizia@gmail.it',
+  ]
+];
+return ($users);
