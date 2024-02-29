e20bd842d49a1de5bb3c7739bdebe2661afc9a13
michelepiccinno <michele.piccinno@gmail.com>
2024-02-29 16:04:29 +0100

    filter orders by restaurants

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller; // Controller di base da importare
 use App\Models\Order;
 use App\Http\Requests\StoreOrderRequest;
 use App\Http\Requests\UpdateOrderRequest;
+use App\Models\Category;
+use App\Models\Restaurant;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
@@ -14,10 +17,21 @@ class OrderController extends Controller
      */
     public function index()
     {
-        $orders = Order::all();
-        return view("admin.orders.index", compact("orders"));
-    }
+        $loggedUserId = Auth::user()->id;
+    
+        $restaurantId = Restaurant::where('user_id', $loggedUserId)->value('id');
 
+        $orders = Order::whereHas('products', function ($query) use ($restaurantId) {
+                $query->where('restaurant_id', $restaurantId);
+            })
+            ->with('products')
+            ->get();
+
+        $categories = Category::all();
+        
+        return view("admin.orders.index", compact("categories", "orders"));
+    }
+    
     /**
      * Show the form for creating a new resource.
      */
