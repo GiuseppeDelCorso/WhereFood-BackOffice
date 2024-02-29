a690a61213c916ca9b9fa40d59cff24cbf8dfdeb
Your Name <you@example.com>
2024-02-29 16:09:27 +0100

    pagina di benvenuto completata

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,26 +17,85 @@
 </div>
 
     <footer>
-        
+        <div class="container-fluid pt-5 ps-5">
+                  <div class="row">
+                <div class="col-3 box">
+                    <h5>Scopri Wherefood</h5>
+                    <ul>
+                        <li>Chi Siamo</li>
+                        <li>Ristoranti</li>
+                        <li>Altro</li>
+                        <li>Lavora con noi</li>
+                    </ul>
+                </div>
+
+                <div class="col-3 box">
+                    <h5>Note Legali</h5>
+                    <ul>
+                        <li>Termini & Condizioni</li>
+                        <li>Informativa sulla privacy</li>
+                        <li>Cookies</li>
+                    </ul>
+                </div>
+                
+                <div class="col-3 box">
+                    <h5>Aiuto</h5>
+                    <ul>
+                        <li>FAQ</li>
+                        <li>Contatti</li>
+                        <li>Tipi di cucina</li>
+                    </ul>
+                </div>
+                
+                    <div class="col-3 box">
+                        <h5>Porta Wherefood con te</h5>
+                        <button type="button" class="btn btn-light">Dowload</button>
+                    </div>
+              </div>
+            </div>
+
+            <div class="firma">
+                Progettato da Andrea Francioni, Giuseppe Del Corso, Michele Piccillo, Emanuele Guerra, Emanuele Khushi
+            </div>
     </footer>
-@endsection
-
-
-<style>.jumbo{
-      font-family: Arial, Helvetica, sans-serif;
-      color: white;
-      height: 87%;
+    
+    
+    @endsection
+    
+    
+    <style>.jumbo{
+        font-family: Arial, Helvetica, sans-serif;
+        color: white;
+        height: 87%;
     }</style>
 
 <style>img{
     width: 33%;
-  }</style>
+}</style>
 
 <style>.hamburger{
     padding-top: 25%;
     width: 30%;
-  }</style>
+}</style>
 
 <style>.back{
     background-image: url(/storage/welcome-img/back.jpeg);
+}</style>
+
+<style>footer{
+    color: white;
+    background-color: #222222;
+    font-family: Arial, Helvetica, sans-serif;
+}</style>
+
+<style>li{
+    list-style: none;
+    position: relative;
+    right: 13%;
+  }</style>
+
+<style>.firma{
+    padding-top: 5%;
+    text-align: center;
+    opacity: 40%;
   }</style>
