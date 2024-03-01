1e1e42e2e9c48f6b9d502c40f33d6ec671a79881
michelepiccinno <michele.piccinno@gmail.com>
2024-03-01 23:29:08 +0100

    add products and price (to order details)

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.admin')
 
 @section('content')
+    {{--     <pre>
+        @php
+            var_dump($orders[0]->products[0]->name);
+        @endphp
+    </pre> --}}
+
     <div class="container-fluid mt-4">
         <div class="row justify-content-center">
             {{-- Crea card per prodotti --}}
@@ -14,8 +20,11 @@
                         <div class="card-body">Cognome: {{ $order->customer_surname }}</div>
                         <div class="card-body">Indirizzo: {{ $order->customer_address }}</div>
                         <div class="card-body">Numero: {{ $order->customer_number }} </div>
+                        @foreach ($order->products as $product)
+                            <div class="card-body">Prodotto: {{ $product->name }} {{ $product->price }}</div>
+                        @endforeach
                         <div class="card-body">Prezzo Totale: {{ $order->total_order }} </div>
-                         <div class="card-body">Stato del Pagamento: {{ $order->state_payment }} </div>
+                        <div class="card-body">Stato del Pagamento: {{ $order->state_payment }} </div>
                     </div>
                 </div>
             @endforeach
@@ -24,10 +33,11 @@
 @endsection
 
 <style>
-    .bgCard{
+    .bgCard {
         background-color: rgb(188, 226, 228) !important;
     }
-    .FontStyleName{
+
+    .FontStyleName {
         font-weight: bold;
     }
 </style>
