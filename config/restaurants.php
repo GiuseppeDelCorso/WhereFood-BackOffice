9b08ee739f2ab54df10090d12168edabb895caff
michelepiccinno <michele.piccinno@gmail.com>
2024-03-01 15:38:50 +0100

    fix restaurant.php

--- config/restaurants.php
+++ config/restaurants.php
@@ -16,18 +16,53 @@ $restaurants = [
     'PIVA' => '90214930960'
   ],
   [
-    'user_id' => 14,
-    'name' => 'FiveGuys',
-    'image' => 'immagine_FiveGuys',
-    'address' => 'Via Alessandria 2',
-    'PIVA' => '90214930960'
-  ],
-  [
     'user_id' => 3,
     'name' => 'Pizzeria da Gigi',
     'image' => 'immagine_PizzeriaGigi',
     'address' => 'Viale Quarta 98',
     'PIVA' => '19814981944'
+  ],
+  [
+    'user_id' => 4,
+    'name' => 'Steak House',
+    'image' => 'immagine_steakhouse',
+    'address' => 'Via Lucca 45',
+    'PIVA' => '95214961967'
+  ],
+  [
+    'user_id' => 5,
+    'name' => 'La Taverna Del Pescatore',
+    'image' => 'immagine_tavernapescatore',
+    'address' => 'Via Lodi 35',
+    'PIVA' => '88214961961'
+  ],
+  [
+    'user_id' => 6,
+    'name' => 'Il Kebab di Mubarak',
+    'image' => 'immagine_kebabmubarak',
+    'address' => 'Vico dei tori 56',
+    'PIVA' => '87314981555'
+  ],
+  [
+    'user_id' => 7,
+    'name' => 'Hokkaido',
+    'image' => 'immagine_hokkaido',
+    'address' => 'Viale Umbria 13',
+    'PIVA' => '11224981557'
+  ],
+  [
+    'user_id' => 8,
+    'name' => 'Ristorante Vegano Piccapane',
+    'image' => 'immagine_piccapane',
+    'address' => 'Viale del Salento 21',
+    'PIVA' => '55524981575'
+  ],
+  [
+    'user_id' => 9,
+    'name' => 'Ristorante Cor di zia',
+    'image' => 'immagine_cor di zia',
+    'address' => 'Viale abrosia',
+    'PIVA' => '56524982573'
   ],
   [
     'user_id' => 10,
@@ -58,32 +93,11 @@ $restaurants = [
     'PIVA' => '19814981944'
   ],
   [
-    'user_id' => 6,
-    'name' => 'Il Kebab di Mubarak',
-    'image' => 'immagine_kebabmubarak',
-    'address' => 'Vico dei tori 56',
-    'PIVA' => '87314981555'
-  ],
-  [
-    'user_id' => 7,
-    'name' => 'Hokkaido',
-    'image' => 'immagine_hokkaido',
-    'address' => 'Viale Umbria 13',
-    'PIVA' => '11224981557'
-  ],
-  [
-    'user_id' => 8,
-    'name' => 'Ristorante Vegano Piccapane',
-    'image' => 'immagine_piccapane',
-    'address' => 'Viale del Salento 21',
-    'PIVA' => '55524981575'
-  ],
-  [
-    'user_id' => 9,
-    'name' => 'Ristorante Cor di zia',
-    'image' => 'immagine_cor di zia',
-    'address' => 'Viale abrosia',
-    'PIVA' => '56524982573'
-  ],
+    'user_id' => 14,
+    'name' => 'FiveGuys',
+    'image' => 'immagine_FiveGuys',
+    'address' => 'Via Alessandria 2',
+    'PIVA' => '90214930960'
+  ]
 ];
 return ($restaurants);
