f553504fb238e220862fb9f4f2acf512099e3c48
andreafrancioni <andreafrancioni.work@gmail.com>
2024-03-06 16:04:35 +0100

    created order controller api and post route

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Order;
+
+class OrderController extends Controller
+{
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'customer_name' => 'required|string',
+            'customer_surname' => 'required|string',
+            'customer_address' => 'required|string',
+            'customer_number' => 'required|string',
+            'total_order' => 'string',
+            'state_payment' => 'boolean',
+        ]);
+
+        $order = Order::create($validatedData);
+
+        return response()->json($order, 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RestaurantController;
-use App\Http\Controllers\Api\ProductControllerController;
-use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +32,9 @@ Route::get('restaurants/categories/{category_id}', [RestaurantController::class,
 
 // Rotte per visualizzare l'elenco delle categorie
 Route::get('categories', [CategoryController::class, "index"]);
+
+// Rotta per la creazione dell'ordine
+Route::post('/orders', [OrderController::class, "store"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
