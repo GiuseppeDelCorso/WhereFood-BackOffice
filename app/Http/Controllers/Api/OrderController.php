182924dfec4708ada440eeef29bd55912b3b9690
andreafrancioni <andreafrancioni.work@gmail.com>
2024-03-07 23:38:12 +0100

    Add order creation from vue axios, and link in pivot table with order id and products id for every product and order

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -10,17 +10,31 @@ class OrderController extends Controller
 {
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
+        $request->validate([
             'customer_name' => 'required|string',
             'customer_surname' => 'required|string',
             'customer_address' => 'required|string',
             'customer_number' => 'required|string',
             'total_order' => 'required|numeric|between:0,99.99',
             'state_payment' => 'boolean',
+            'products' => 'required|array',
+            'products.*.id' => 'required|exists:products,id',
+            'products.*.quantity' => 'required|integer|min:1',
         ]);
 
-        $order = Order::create($validatedData);
+        $order = Order::create([
+            'customer_name' => $request->input('customer_name'),
+            'customer_surname' => $request->input('customer_surname'),
+            'customer_address' => $request->input('customer_address'),
+            'customer_number' => $request->input('customer_number'),
+            'total_order' => $request->input('total_order'),
+            'state_payment' => $request->input('state_payment'),
+        ]);
 
-        return response()->json($order, 201);
+        foreach ($request->input('products') as $product) {
+            $order->products()->attach($product['id'], ['quantity' => $product['quantity']]);
+        }
+
+        return response()->json(['message' => 'Order created successfully', 'order' => $order], 201);
     }
 }
